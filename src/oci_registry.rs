bcbb9caa6c96bcad3c87b29e8b8f2f5cdaeb8312
juv <8269353+juv@users.noreply.github.com>
2025-10-05 20:34:51 +0300

    Implement OAuth token retrieval (#55)
    * Working draft for OAuth token retrieval
    
    * Clean up code
    
    * Clean up code
    
    * Remove unused import

--- src/oci_registry.rs
+++ src/oci_registry.rs
@@ -1,12 +1,23 @@
 use crate::config::RegistrySecret::{ImagePullSecret, Opaque};
 use crate::config::{Config, RegistrySecret};
 use crate::image_reference::ImageReference;
+use crate::secret_string::SecretString;
 use anyhow::{Context, Result};
-use axum::http::HeaderMap;
-use reqwest::header::{ACCEPT, AUTHORIZATION};
+use axum::http::{HeaderMap, StatusCode};
+use reqwest::header::{ACCEPT, AUTHORIZATION, WWW_AUTHENTICATE};
 use reqwest::{Certificate, Client, Response};
+use serde::Deserialize;
+use std::collections::HashMap;
 use std::fs;
 use tracing::{debug, info};
+
+static OCI_ACCEPT_HEADER: &str =
+    "application/vnd.oci.image.manifest.v1+json, application/vnd.oci.image.index.v1+json";
+
+#[derive(Deserialize)]
+struct RegistryTokenResponse {
+    token: String,
+}
 
 pub fn create_client(config: &Config) -> Result<Client> {
     info!("Initializing OCI Registry HTTP client");
@@ -39,37 +50,73 @@ pub async fn fetch_digest_from_tag(
         registry, image_reference.repository, image_reference.tag
     );
 
-    let response = fetch_docker_manifest(client, image_reference, registry_secret, &url)
+    let response = fetch_docker_manifest(client, registry_secret, &url)
         .await
         .with_context(|| format!("Failed to fetch manifest from {}", url))?;
 
-    if let Ok(digest) = get_digest_from_response(&response) {
-        return Ok(digest);
-    }
+    match response.status() {
+        StatusCode::OK => {
+            let digest = get_digest_from_response(&response)?;
+            return Ok(digest);
+        }
 
-    if enable_jfrog_artifactory_fallback {
-        if is_artifactory_response(&response.headers()) {
-            let fallback_url = get_artifactory_fallback_url(image_reference, registry);
-            info!(
-                "Received http status {} previously, fetching digest from Artifactory fallback url {}",
-                response.status(),
-                fallback_url
-            );
+        StatusCode::UNAUTHORIZED => {
+            if response.headers().contains_key(WWW_AUTHENTICATE) {
+                let www_authenticate_header = response
+                    .headers()
+                    .get(WWW_AUTHENTICATE)
+                    .expect(&format!(
+                        "Missing header {} from registry {}",
+                        WWW_AUTHENTICATE, registry
+                    ))
+                    .to_str()?;
 
-            let response =
-                fetch_docker_manifest(client, image_reference, registry_secret, &fallback_url)
+                let registry_secret = handle_oauth_authentication_challenge(
+                    client,
+                    registry,
+                    registry_secret,
+                    www_authenticate_header,
+                )
+                .await
+                .context("Failed to fetch OAuth token from")?;
+
+                let response = fetch_docker_manifest(client, &registry_secret, &url)
+                    .await
+                    .with_context(|| format!("Failed to fetch manifest from {}", url))?;
+
+                debug!("Authentication challenge response: {:?}", response);
+
+                let digest = get_digest_from_response(&response)?;
+                return Ok(digest);
+            }
+        }
+
+        StatusCode::NOT_FOUND => {
+            if enable_jfrog_artifactory_fallback && is_artifactory_response(&response.headers()) {
+                let fallback_url = get_artifactory_fallback_url(image_reference, registry);
+                info!(
+                    "Received http status {} previously, fetching digest from Artifactory fallback url {}",
+                    response.status(),
+                    fallback_url
+                );
+
+                let response = fetch_docker_manifest(client, registry_secret, &fallback_url)
                     .await
                     .context(format!(
                         "Failed to fetch manifest from Artifactory fallback url {}",
                         fallback_url
                     ))?;
 
-            let digest = get_digest_from_response(&response).context("Failed to re")?;
-            return Ok(digest);
-        } else {
+                let digest = get_digest_from_response(&response)?;
+                return Ok(digest);
+            }
+        }
+
+        status => {
             anyhow::bail!(
-                "Artifactory fallback is enabled but no Artifactory indicators were found in response headers from {}",
-                registry
+                "Registry {} returned error status {} while fetching OCI image manifest",
+                image_reference.registry,
+                status
             );
         }
     }
@@ -82,30 +129,24 @@ pub async fn fetch_digest_from_tag(
 
 async fn fetch_docker_manifest(
     client: &Client,
-    image_reference: &ImageReference,
     registry_secret: &RegistrySecret,
     url: &str,
 ) -> Result<Response> {
     info!("Fetching docker manifest for from URL {}", url);
 
     let authorization_header = get_authorization_header(registry_secret);
-    info!("Using authorization header {}", authorization_header);
+    debug!("Authorization header: {}", authorization_header);
 
     let response = client
         .get(url)
-        .header(ACCEPT, "application/vnd.oci.image.manifest.v1+json")
+        .header(ACCEPT, OCI_ACCEPT_HEADER)
         .header(AUTHORIZATION, authorization_header)
         .send()
         .await
         .context("Failed to send request to fetch manifest")?;
 
-    if !response.status().is_success() {
-        anyhow::bail!(
-            "Registry {} returned error status {} while fetching OCI image manifest",
-            image_reference.registry,
-            response.status()
-        );
-    }
+    debug!("Fetch Docker manifest response: {:?}", response);
+
     Ok(response)
 }
 
@@ -149,11 +190,84 @@ fn get_authorization_header(registry_secret: &RegistrySecret) -> String {
     match registry_secret {
         Opaque { token, .. } => format!("Bearer {}", token.expose_secret()),
         ImagePullSecret { docker_config, .. } => {
-            format!(
-                "Basic {}",
-                docker_config.auths.iter().next().unwrap().1.auth
-            )
+            let first_docker_config = docker_config.auths.iter().next().unwrap();
+            let docker_secret = &first_docker_config.1.auth;
+            format!("Basic {}", docker_secret.expose_secret())
         }
         RegistrySecret::None => String::new(),
+    }
+}
+
+async fn handle_oauth_authentication_challenge(
+    client: &Client,
+    registry: &str,
+    registry_secret: &RegistrySecret,
+    www_authenticate_header: &str,
+) -> Result<RegistrySecret> {
+    debug!(
+        "Trying to parse WWW-Authenticate header response from registry {}: {}",
+        registry, www_authenticate_header
+    );
+
+    //parse auth challenge information from WWW-Authenticate header: https://datatracker.ietf.org/doc/html/rfc6750#section-3
+    //example: WWW-Authenticate: Bearer realm="https://auth.docker.io/token",service="registry.docker.io",scope="repository:samalba/my-app:pull,push"
+    let auth_challenge_params: Vec<_> = www_authenticate_header[7..].split(',').collect();
+    let mut auth_challenge_map: HashMap<_, _> = auth_challenge_params
+        .iter()
+        .filter_map(|field| {
+            let mut parts = field.splitn(2, '=');
+            let key = parts.next()?.trim().trim_matches('"');
+            let value = parts.next()?.trim().trim_matches('"');
+            Some((key, value))
+        })
+        .collect();
+
+    let realm = auth_challenge_map.remove("realm").context(format!(
+        "Expected missing field realm in WWW-Authenticate challenge from {}",
+        registry
+    ))?;
+    let service = auth_challenge_map.get("service").context(format!(
+        "Expected missing field service in WWW-Authenticate challenge from {}",
+        registry
+    ))?;
+    let scope = auth_challenge_map.get("scope").context(format!(
+        "Expected missing field scope in WWW-Authenticate challenge from {}",
+        registry
+    ))?;
+
+    info!(
+        "Requesting authentication token from {} for service {} and scope {}",
+        realm, service, scope
+    );
+
+    let token_url = format!("{}?service={}&scope={}", realm, service, scope);
+    let token_response = client
+        .get(&token_url)
+        .header(AUTHORIZATION, get_authorization_header(registry_secret))
+        .send()
+        .await
+        .context("Failed to get token from registry")?;
+
+    match token_response.status() {
+        StatusCode::OK => {
+            let token_content = token_response
+                .json::<RegistryTokenResponse>()
+                .await
+                .context("Failed to parse JSON response from registry")?;
+
+            let registry_secret = RegistrySecret::Opaque {
+                username: None,
+                token: SecretString::new(token_content.token),
+            };
+            Ok(registry_secret)
+        }
+
+        status => {
+            anyhow::bail!(
+                "Failed to retrieve OAuth authentication token from {}, error code {}",
+                realm,
+                status
+            );
+        }
     }
 }
